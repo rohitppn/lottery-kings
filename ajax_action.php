4da4e55484cacf79848d43b63f7f7c885462ff5c
Rohit Sharma <mail2rohitsharmappn@gmail.com>
2023-10-11 09:17:07 +0530

    frst commit

---
+++ ajax_action.php
@@ -0,0 +1,33 @@
+	<table class="table table-striped ">
+		<tbody>
+			<tr>
+				<th><strong>Id</strong></th>
+				<th><strong>Ticket No</strong></th>
+				<th><strong>Price</strong></th>
+				<th><strong>Quantity</strong></th>
+				<th><strong>Action</strong></th>
+			</tr>
+			<tr>
+				<td><strong>3</strong></td>
+				<td>KL01966</td>
+				<td>Rs. 40</td>
+				<td>1</td>
+				<td><a onClick="cartAction('remove','KL01966')" class="btnRemoveAction btn btn-danger">Remove Item</a></td>
+			</tr>
+			<tr>
+				<td><strong>32</strong></td>
+				<td>KL18872</td>
+				<td>Rs. 40</td>
+				<td>1</td>
+				<td><a onClick="cartAction('remove','KL18872')" class="btnRemoveAction btn btn-danger">Remove Item</a></td>
+			</tr>
+
+
+			<tr>
+				<td colspan="5" align=right><strong>Total:</strong> Rs. 80</td>
+			</tr>
+			<tr>
+				<td colspan="5" align="right"><a onClick="cartAction('empty','')" class="btnAdded btn btn-info py-2">Clear Cart</a> <a href="https://www.bookyourlottery.com/cart" class="btnAdded buynow btn btn-success py-2">Buy Now</a><a href="https://www.bookyourlottery.com/" class=" btn btn-warning addmore py-2">Add More</a></td>
+			</tr>
+		</tbody>
+	</table>
